e8e78978a03f830209b65f504a94669eac9a8157
Samir Samawade <samirsamawade@gmail.com>
2023-01-07 19:43:04 +0300

    User Exceptions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Delete, Param, Query } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -14,8 +14,13 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string){
-    return this.userService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string){
+    const user= await this.userService.findOne(parseInt(id));
+    if(!user){
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 
   @Get()

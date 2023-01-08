3bbed6b64d6902b25d99f75a4a97ed74f72aed61
Samir Samawade <samirsamawade@gmail.com>
2023-01-08 11:18:30 +0300

    Custom Data Serialization

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,29 @@
+import { 
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler
+  } from "@nestjs/common";
+
+  import { Observable } from "rxjs";
+  import { map } from "rxjs/operators";
+
+  import { plainToClass } from "class-transformer";
+
+  export class SerializeInterceptor implements NestInterceptor{
+    intercept(context: ExecutionContext, handler: CallHandler): Observable<any>  {
+      //Run something before request is handled
+      //by the request handler
+      console.log('I am running before the handler', context);
+
+      return handler.handle().pipe(
+        map((data: any) =>{
+          //Run something before the response is sent out
+          console.log('I am running before the response is sent out', data);
+        })
+      )
+
+      throw new Error("Method not implemented.");
+    }
+    
+  }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,25 @@
-import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from './../interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
 
-  constructor(private userService: UsersService){}
+  constructor(private userService: UsersService) { }
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto){
+  createUser(@Body() body: CreateUserDto) {
     this.userService.create(body.email, body.password);
   }
 
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
-  async findUser(@Param('id') id: string){
-    const user= await this.userService.findOne(parseInt(id));
-    if(!user){
+  async findUser(@Param('id') id: string) {
+    console.log('Handler is running');
+    const user = await this.userService.findOne(parseInt(id));
+    if (!user) {
       throw new NotFoundException('User not found');
     }
 
@@ -24,17 +27,17 @@ export class UsersController {
   }
 
   @Get()
-  findAllUsers(@Query('email') email: string){
+  findAllUsers(@Query('email') email: string) {
     return this.userService.find(email);
   }
 
   @Delete('/:id')
-  removeUser(@Param('id') id: string){
+  removeUser(@Param('id') id: string) {
     return this.userService.remove(parseInt(id));
   }
 
   @Patch('/:id')
-  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.userService.update(parseInt(id), body);
   }
 }

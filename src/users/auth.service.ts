53c36bc6fb6fced5228156a932890fdb9d6b4c4d
Samir Samawade <samirsamawade@gmail.com>
2023-01-09 15:39:59 +0300

    Creating a User

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -14,13 +14,18 @@ export class AuthService {
 
     if (users.length) {
       throw new BadRequestException('Email already in use');
+    }
 
       const salt = randomBytes(8).toString('hex');
 
       const hash = (await scrypt(password, salt, 32)) as Buffer;
 
       const result = salt + '.' + hash.toString('hex');
-    }
+
+      const user = await this.userService.create(email, result);
+
+      return user;
+    
   }
 
   signin(){}

44b1242542a3f6aa34fa534d96daec7a0e9d8950
samir samawade <samirsamawade@gmail.com>
2023-01-15 09:51:29 +0300

    Testing the Signin

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,14 +1,16 @@
 import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { User } from "./user.entity";
-
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { UsersService } from "./users.service";
 
 describe('AuthService', ()=>{
     let service : AuthService;
 
+    let fakeUsersService: Partial<UsersService>;
+
 beforeEach( async ()=>{
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
         find: () => Promise.resolve([]),
         create: (email: string, password: string)=>
             Promise.resolve({ id: 1, email, password } as User)
@@ -37,6 +39,21 @@ it('create a new user with a salted and hashed password', async ()=>{
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
-})
+});
+
+it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
+  });
+
 
 });

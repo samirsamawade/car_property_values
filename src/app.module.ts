9df8ca88f8ce0d9f3b4f0d376d16eedd254f556d
samir samawade <samirsamawade@gmail.com>
2023-01-26 17:03:14 +0300

    App Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@
 import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -12,12 +13,30 @@ const cookieSession = require('cookie-session');
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true,
-  }) , UsersModule, ReportsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+            type: 'sqlite',
+            database: config.get<string>('DB_NAME'),
+            entities: [User, Report],
+            synchronize: true,
+          }
+        }
+    }),
+  //   TypeOrmModule.forRoot({
+  //   type: 'sqlite',
+  //   database: 'db.sqlite',
+  //   entities: [User, Report],
+  //   synchronize: true,
+  // }) 
+   UsersModule, ReportsModule
+],
   controllers: [AppController],
   providers: [
     AppService,
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -20,12 +20,33 @@ describe('Authentication System', () => {
 
     return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({email, password: '1234'})
+      .send({ email, password: '1234' })
       .expect(201)
-      .then((res)=>{
-        const {id, email} = res.body;
+      .then((res) => {
+        const { id, email } = res.body;
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+
   });
+
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '1234' })
+      .expect(201)
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(body.email).toEqual(email);
+  });
+
 });

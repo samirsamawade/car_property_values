804e1373d47e1fbb21038a389e53ce8bcd1a119e
samir samawade <samirsamawade@gmail.com>
2023-02-02 14:36:25 +0300

    Create Report

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -7,4 +7,21 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number;
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
 }

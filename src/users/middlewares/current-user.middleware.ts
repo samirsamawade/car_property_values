3377892d4de5f355518ee19346c0b4533315528c
samir samawade <samirsamawade@gmail.com>
2023-02-04 14:25:23 +0300

    Assigning CurrentUser with a Middleware

---
+++ src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,21 @@
+import { NestMiddleware, Injectable } from "@nestjs/common";
+import { Request, Response, NextFunction } from "express";
+import { UsersService } from "../users.service";
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware{
+    constructor (private usersService: UsersService){}
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        const {userId} = req.session || {};
+        if(userId){
+            const user = await this.usersService.findOne(userId);
+
+            //@ts-ignore
+            req.CurrentUser = user;
+        }
+
+        next();
+    }
+    
+}
